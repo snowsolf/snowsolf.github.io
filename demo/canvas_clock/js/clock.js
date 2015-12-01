3592b1dd800600fd8a11a19062002faa79fa2e0e
snowsolf <snowsolf@users.noreply.github.com>
2015-12-01 23:33:53 +0800

    Update clock.js

--- demo/canvas_clock/js/clock.js
+++ demo/canvas_clock/js/clock.js
@@ -13,7 +13,7 @@ var y = 105;
  * 钟表半径
  * @type {number}
  */
-var radius = 90;
+var radius = 190;
 var pi = Math.PI;
 window.onload = function() {
 	var canvas = document.getElementById('canvas');
